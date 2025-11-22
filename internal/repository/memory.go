6c19896e66e762ecc4c6941825ed79fbc576fc5a
oleg2210 <dmchnklg@gmail.com>
2025-11-22 22:41:38 +0500

    saving to file

--- internal/repository/memory.go
+++ internal/repository/memory.go
@@ -1,13 +1,62 @@
 package repository
 
+import (
+	"encoding/json"
+	"os"
+)
+
+type record struct {
+	UUID        string `json:"uuid"`
+	ShortURL    string `json:"short_url"`
+	OriginalURL string `json:"original_url"`
+}
+
 type MemoryRepository struct {
 	data map[string]string
+	path string
 }
 
 func NewMemoryRepository(fileStoragePath string) *MemoryRepository {
-	return &MemoryRepository{
+	repo := &MemoryRepository{
 		data: make(map[string]string),
 	}
+
+	repo.loadDataFromFile()
+	return repo
+}
+
+func (repo *MemoryRepository) loadDataFromFile() {
+	bytes, err := os.ReadFile(repo.path)
+	if err != nil {
+		return
+	}
+
+	var records []record
+	if err := json.Unmarshal(bytes, &records); err != nil {
+		return
+	}
+
+	for _, r := range records {
+		repo.data[r.ShortURL] = r.OriginalURL
+	}
+}
+
+func (repo *MemoryRepository) saveToFile() error {
+	records := make([]record, 0, len(repo.data))
+	for short, original := range repo.data {
+		records = append(records, record{
+			UUID:        short,
+			ShortURL:    short,
+			OriginalURL: original,
+		})
+	}
+
+	bytes, err := json.MarshalIndent(records, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(repo.path, bytes, 0644)
 }
 
 func (repo *MemoryRepository) Save(id string, url string) error {
@@ -17,6 +66,7 @@ func (repo *MemoryRepository) Save(id string, url string) error {
 	}
 
 	repo.data[id] = url
+	repo.saveToFile()
 	return nil
 }
 
